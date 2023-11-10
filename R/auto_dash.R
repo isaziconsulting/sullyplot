00277e0aa1c219085abb58d96018aaf751682686
Kelian Massa <kelian@isaziconsulting.co.za>
2023-11-10 13:18:33 +0000

    Optional custom dash prompting

--- R/auto_dash.R
+++ R/auto_dash.R
@@ -5,6 +5,7 @@
 #'
 #' @param file_path The path to your input file. Must be .csv or .xlsx.
 #' @param num_plots The number of plots in your dashboard. Default is 4.
+#' @param custom_description An optional description to describe the custom dashboard you want.
 #' @param dash_model The name of the language model to use for designing the dashboard. Default 'gpt-4'.
 #' @param code_model The name of the language model to use for coding individual plots. Default 'gpt-4'.
 #' @param num_design_attempts The number of iterations to improve on the dashboard design. Default is 1.
@@ -17,8 +18,9 @@
 #'
 #' @examples
 #' \dontrun{
-#' # Example usage with saving ggplots to a pdf
-#' all_plots <- auto_dash("iris.csv")
+#' # Example usage of a custom dashboard with saving list of ggplots to a pdf
+#' all_plots <- auto_dash("auto-dash/kaggle_data/train/iris.csv", num_design_attempts=1, save_messages=TRUE, save_dir="sullyplot_messages", save_name="iris_custom",
+#' custom_description="Two box plots for lengths and widths (use two y axes per plot to show petal and sepal lengths/widths together), and 2 scatter plots of lengths vs widths. All should be grouped by variety.")
 #' pdf("my_dash.pdf", width = 20, height = 10 * length(all_plots) / 2)
 #' do.call(grid.arrange, c(all_plots, ncol = 2))
 #' dev.off()
@@ -26,25 +28,31 @@
 #' 
 #' @importFrom rlang .data
 #' @export
-auto_dash <- function(file_path, num_plots = 4, dash_model="gpt-4", code_model="gpt-4", num_design_attempts=1, num_code_attempts=5, save_messages=FALSE, save_dir="", save_name="auto_dash") {
+auto_dash <- function(file_path, num_plots = 4, custom_description="", dash_model="gpt-4", code_model="gpt-4", num_design_attempts=1, num_code_attempts=5, save_messages=FALSE, save_dir="", save_name="auto_dash") {
   file_df <- read_file(file_path)
   summary <- summarise_df(file_df, remove_cols = TRUE, max_cols = max_cols)
   file_df <- summary$clean_df
   summary_df <- summary$df_stats
   
-  # Get GPT to design and describe the overall dashboard
-  user_prompt <- sprintf(describe_dashboard_prompt, num_plots, to_csv(summary_df), mi_matrix(file_df), significant_categorical_relationships(file_df, summary_df), significant_categorical_numeric_relationships(file_df, summary_df))
-  # user_prompt <- sprintf(describe_dashboard_prompt, num_plots, to_csv(summary_df))
+  # Get GPT to design and describe the overall dashboard, use the custom description if available
+  if(custom_description == "") {
+    user_prompt <- sprintf(describe_dashboard_prompt, num_plots, to_csv(summary_df), mi_matrix(file_df), significant_categorical_relationships(file_df, summary_df), significant_categorical_numeric_relationships(file_df, summary_df))
+  } else {
+    user_prompt <- sprintf(describe_custom_dashboard_prompt, num_plots, custom_description, to_csv(summary_df))
+  }
+    # user_prompt <- sprintf(describe_dashboard_prompt, num_plots, to_csv(summary_df))
   log(user_prompt)
   chat_messages <- data.frame(role = "user", content = user_prompt)
   response_json <- continue_chat(chat_messages, system_message = system_prompt, model_name = dash_model, max_tokens = 768, options = list(temperature = 0))
   log("Initial dashboard design")
   log(response_json)
-  for(attempt_idx in 1:num_design_attempts) {
-    log(sprintf("Improved dashboard design v%d", attempt_idx))
-    chat_messages_new <- rbind(chat_messages, data.frame(role = "user", content = sprintf(improve_dashboard_prompt, response_json)))
-    response_json <- continue_chat(chat_messages_new, system_message = system_prompt, model_name = dash_model, max_tokens = 768, options = list(temperature = 0))
-    log(response_json)
+  if (num_design_attempts > 0) {
+    for(attempt_idx in 1:num_design_attempts) {
+      log(sprintf("Improved dashboard design v%d", attempt_idx))
+      chat_messages_new <- rbind(chat_messages, data.frame(role = "user", content = sprintf(improve_dashboard_prompt, num_plots, response_json)))
+      response_json <- continue_chat(chat_messages_new, system_message = system_prompt, model_name = dash_model, max_tokens = 768, options = list(temperature = 0))
+      log(response_json)
+    }
   }
   plot_info <- fromJSON(response_json, simplifyDataFrame = FALSE, simplifyMatrix = FALSE)
   if(save_messages) {
@@ -54,8 +62,13 @@ auto_dash <- function(file_path, num_plots = 4, dash_model="gpt-4", code_model="
   # Start coding the dashboard
   all_plots <- lapply(seq_len(num_plots), function(idx) {
     log(sprintf("\nGenerating plot %d using the columns: %s", idx, paste(plot_info$input_columns[[idx]], collapse = ", ")))
-    auto_plot_results <- auto_plot(file_df, plot_info$input_columns[[idx]], plot_info$descriptions[[idx]], num_code_attempts, code_model, save_messages, save_dir, sprintf("%s_plot_%d", save_name, idx))
-    return(auto_plot_results$plot_obj)
+    tryCatch({
+      auto_plot_results <- auto_plot(file_df, plot_info$input_columns[[idx]], plot_info$descriptions[[idx]], num_code_attempts, code_model, save_messages, save_dir, sprintf("%s_plot_%d", save_name, idx))
+      return(auto_plot_results$plot_obj)
+    }, error = function(e) {
+      log(sprintf("Failed to generate plot %d: ", idx, e$message))
+      return(NULL)
+    })
   })
   return(all_plots)
 }
--- R/prompts.R
+++ R/prompts.R
@@ -30,14 +30,14 @@ Here is a summary of the columns in my input dataframe:
 describe_dashboard_prompt <- "Identify %d plots for a comprehensive data analysis and exploration dashboard using the data I give you. 
 
 Explore the dataset with a variety of data analysis plots, ensure each plot captures significant and insightful relationships or patterns within the dataset. Choose plots that give a broad and diverse view of the dataset, highlighting different aspects and relationships.
-Ensure there is never more than two of a plot type (e.g. 2 box plots, 2 histograms, 2 scatter plots)
+Ensure there is never more than two of a plot type.
 
 Make sure your plots follow the available plot types & rules:
   Note for all cases where I refer to quantitatives, they must have n_distinct > 20; and for categoricals, they must have they type 'Categorical'
   Here are the available plot types & rules:
     scatter plots - must have quantitatives on the x and y axes, and an optional 3rd categorical or quantitative column for colour.
     line plot - must have a DateTime column on the x-axis, a quantitative on the y axis, and an optional 3rd categorical column for colour.
-    box plots - must have a categorical on the x axis and a quantitative on the y axis. Can have multiple axes if related.
+    box plots - must have a categorical on the x axis and a quantitative on the y axis. Can have multiple quantitative y-axes if related, but you must explicitly state how they should be used together (e.g. separate y-axes or an additional category for which quantitative it is).
     histogram - must have a quantitative on the x-axis in bins, counts on the y axis, and an optional 2nd categorical column for stacked bars, multiple axes, or colour.
     bar chart - must have a categorical on the x-axis and counts on the y axis, and an optional 3rd categorical column for stacked bars or multiple axes.
 
@@ -45,7 +45,7 @@ Make sure your plots follow the available plot types & rules:
 
 Your answer must only be a JSON string with the following keys:
 - \"input_columns\": A list of lists of input columns from the summary needed for each plot.
-- \"descriptions\": A list of plot descriptions explaining each plot and how it must show the relevant relationship.
+- \"descriptions\": A list of plot descriptions explaining each plot, how it must show the relevant relationship, and how each column should be used in the plot.
 
 Here is a summary of the columns in my input dataframe:
 %s
@@ -61,24 +61,51 @@ Significant categorical-to-numeric relationships and ANOVA values:
 
 ** Only output the JSON string; omit any surrounding formatting such as single or double inverted commas or backticks, or stating json ```.**"
 
+describe_custom_dashboard_prompt <- "Identify %d plots for this dashboard:
+%s\n 
+
+Make sure your plots follow the available plot types & rules:
+  Note for all cases where I refer to quantitatives, they must have n_distinct > 20; and for categoricals, they must have they type 'Categorical'
+  Here are the available plot types & rules:
+    scatter plots - must have quantitatives on the x and y axes, and an optional 3rd categorical or quantitative column for colour.
+    line plot - must have a DateTime column on the x-axis, a quantitative on the y axis, and an optional 3rd categorical column for colour.
+    box plots - must have a categorical on the x axis and a quantitative on the y axis. Can have multiple quantitative y-axes if related, but you must explicitly state how they should be used together (e.g. separate y-axes or an additional category for which quantitative it is).
+    histogram - must have a quantitative on the x-axis in bins, counts on the y axis, and an optional 2nd categorical column for stacked bars, multiple axes, or colour.
+    bar chart - must have a categorical on the x-axis and counts on the y axis, and an optional 3rd categorical column for stacked bars or multiple axes.
+
+** Make sure all plots can be plotted as `ggplot` objects. **
+
+Your answer must only be a JSON string with the following keys:
+- \"input_columns\": A list of lists of input columns from the summary needed for each plot.
+- \"descriptions\": A list of plot descriptions explaining each plot, how it must show the relevant relationship, and how each column should be used in the plot.
+
+Here is a summary of the columns in my input dataframe:
+%s
+
+** Only output the JSON string; omit any surrounding formatting such as single or double inverted commas or backticks, or stating json ```.**"
+
 improve_dashboard_prompt <- "Can you improve on previous response by replacing plot descriptions and their input columns according to the following:
+
+Never reduce the amount information in plots e.g. never separate one plot into two
 
 Most importantly, replace any plots that do not follow the plot rules:
   Here are the available plot types & rules:
     scatter plots - must have quantitatives on the x and y axes, and an optional 3rd categorical or quantitative column for colour.
     line plot - must have a DateTime column on the x-axis, a quantitative on the y axis, and an optional 3rd categorical column for colour.
-    box plots - must have a categorical on the x axis and a quantitative on the y axis. Can have an optional 3rd categorical column for grouped box plots.
+    box plots - must have a categorical on the x axis and a quantitative on the y axis. Can have multiple quantitative y-axes if related, but you must explicitly state how they should be used together (e.g. use separate y-axes or an additional category for which quantitative it is).
     histogram - must have a quantitative on the x-axis in bins, counts on the y axis, and an optional 2nd categorical column for stacked bars, multiple axes, or colour.
     bar chart - must have a categorical on the x-axis and a quantitative or counts on the y axis, and an optional 3rd categorical column for stacked bars or multiple axes.
   Note for all cases where I refer to quantitatives, they must have n_distinct > 20; and for categoricals, they must have they type 'Categorical'
 
 Then, where appropriate and not present already, add or improve existing visual enhancements such as:
 - Add categorical columns to show relationships accross categories where relevant (Pay attention to the optional additional category columns in the plot rules)
-- 95%% prediction elipse per category for scatter plots that have a 3rd categorical column
-- Lines of best fit with 5%% confidence intervals
-- Multiple y axes for each category if there are too many categories
-- Group related plots together and when you do, also add a completely new different plot so the total number of plots does not change - e.g. If there are two box/bar plots with the same categories use multiple y-axes to plot them together.
-- Replace any redundant plots
+- Add 95%% prediction elipses per category for any scatter plots that have a 3rd categorical column
+- Add lines of best fit with 5%% confidence intervals where relevant
+- Add multiple y axes for each category if there are too many categories or if you want two sets of categories.
+- Group related plots together - e.g. If there are two box/bar plots with the same categories use multiple separate y-axes to plot them as a single `ggplot` and add a new scatter plot.
+- Replace any redundant plots with different plots.
+
+Finally, make sure there are still at least %d plots in total and add plots if there aren't.
 
 Your previous response was:
 %s
@@ -91,31 +118,32 @@ generate_code_prompt <- "I want to create this plot: %s
     The function should handle the necessary data transformation, statistical analyses, and plotting within it.
     Make sure to handle NA values.
     Make sure continuous numeric ticks are rounded to 2 decimal places, and numeric x axis ticks are rotated.
-
+    
+    For scatter plots with categoricals, always include the 95%% predictions elipses per category.
     Box plots should always be coloured by category.
-    Stacked bar/histogram plots should have different colours for each stack.
     Make sure to use bins for histograms.
     Never colour histograms by count.
     Multi-category scatter/line plots, should have different colours for each category
-    Prediction elipses should have the same colour as their category with a transparent fill.
+    Prediction elipses should have the same colour as their category.
+    Always use `theme_grey`, the `Set3` colour palette from `RColorBrewer`.
+    Always plot with a colour (never plot in black).
     
-    ** The default theme should be black axes, a white background with grey grid lines, and light blue for data, never plot in black**
     ** Your code must be compatible with only the libraries ggfortify, ggplot2, ggcorrplot, tidyverse, dplyr, broom, Cairo, gridExtra, reshape2, modelr **
     ** Make sure the `plot_df` function returns a `ggplot` object **
     ** DO NOT include comments **
     ** DO NOT include library requirements (only write the code without library() or require() statements) **
     ** Make sure all plots have a concise title and axes are labelled **
     ** Your answer must be just the string of code, without any surrounding formatting like single or double inverted commas or backticks **
-    ** Pay attention to which axis each column is on and never change this (if I say counts on y axi do not change this) **
+    ** Pay attention to which axis each column is on and never change this (if I say count on y axis do not change this) **
     Here is a summary of the columns in the input data frame df:
     %s"
 
 # ** The default theme should be black axes, a white background with grey grid lines, and light blue for data, never plot in black**
-# ** When you need mutliple colours use a colour palette of light blue, pink, light green, yellow, light red and orange for data visualisation **
+# ** When colouring by category, use a colour palette of light blue, pink, light green, yellow, light red and orange **
 
 fix_error_prompt <- "Your previous code: %s
     Failed with: %s
-    Rewrite the function to account for this error and respond with only the new code and nothing else."
+    Rewrite the function so that it does not encounter this error and respond with only the new code and nothing else."
 
 fix_low_quality_plot_prompt <- "Your previous code: %s
     Led to a low quality plot for this reason: %s

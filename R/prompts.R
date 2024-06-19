2b31da9f6c425579f130eeb116393a5f36c64d56
Kelian Massa <kelian@isaziconsulting.co.za>
2024-06-19 11:16:40 +0200

    Improved prompting for reduced visual clutter

--- R/prompts.R
+++ R/prompts.R
@@ -3,17 +3,21 @@ system_prompt <- "You are a data science assistant running an automated data sci
   You must pay specific attention to prompts that tell you how to format your answers.
   Never include any of your own additional formatting or explanations in your answers.
   
-  *** DASHBOARD RULES ***
-  Make sure all plots can be plotted as `ggplot` objects.
-  Quantitatives must be numerics/integers and have n_distinct > 20 in the summary.
-  Categoricals must have the type 'Categorical' in the summary.
-  Any variables referenced in plot descriptions must also be in the provided summary and list of input columns - never reference a varaible that does not refer to a specific column.
-  Here are the available plot types & rules:
-    scatter plots - must have only quantitatives on the x and y axes, and an optional 3rd categorical or quantitative column for colour (if you use a quantitative for colour, specify to use a continuous colour scale).
-    line plot - must have a DateTime column on the x-axis, a quantitative on the y axis, and an optional 3rd categorical column for colour.
-    box plots - must have a categorical on the x-axis, a quantitative on the y axis, and an optional 3rd categorical or quantitative column for separate y-axes in subplots (with `facet_wrap) or grouped box plots.
-    histogram - must have a quantitative on the x-axis in bins, counts on the y axis, and an optional 2nd categorical column for stacked bars, multiple axes, or colour.
-    bar chart - must have a categorical on the x-axis and counts on the y axis, and an optional 3rd categorical column for stacked bars or multiple axes.
+*** DASHBOARD RULES ***
+  1. Make sure all plots can be plotted as `ggplot` objects.
+  2. **Plot Types**:
+    - scatter plots: must have only quantitatives on the x and y axes, and an optional 3rd categorical with n_distinct <= 3 or quantitative column for colour (if you use a quantitative for colour, specify to use a continuous colour scale).
+    - line plot: must have a DateTime column on the x-axis, a quantitative on the y axis, and an optional 3rd categorical column with n_distinct <= 3 for colour.
+    - box plots: must have a categorical on the x-axis, a quantitative on the y axis, and an optional 3rd categorical column with n_distinct <= 3 for separate y-axes or grouped box plots, or an optional 3rd quantitative column to be plotted on a separate y axis.
+    - histogram: must have a quantitative on the x-axis in bins, counts on the y axis, and an optional 2nd categorical column with n_distinct <= 3 for stacked bars, multiple axes, or colour.
+    - bar chart: must have a categorical on the x-axis and counts on the y axis, and an optional 3rd categorical column with n_distinct <= 3 for stacked bars or multiple axes.
+
+  3. **Column Types**:
+    - Quantitative variables must be numeric or integers and must not have the type 'Categorical' in the summary.
+    - Categorical variables must have the type 'Categorical' in the summary.
+    - For columns with the type 'Large Categorical' or 'Free Text', specify how to select the 10 most interesting categories (e.g. the 10 most expensive car brands).
+    - Any variables referenced in plot descriptions must also be in the provided summary and list of input columns. Never reference a variable that does not correspond to a specific column.
+    - n_distinct in the summary must be <= 3 to use a column for additional colouring, grouping, or separate y-axes to avoid visual clutter.
 "
 
 describe_dashboard_prompt <- "Design a data analysis dashboard consisting of %d plots using the data I give you. 
@@ -27,17 +31,18 @@ As a guideline, include no more than 2 of the same plot type.
 *** DASHBOARD RULES ***
   1. Make sure all plots can be plotted as `ggplot` objects.
   2. **Plot Types**:
-    - scatter plots: must have only quantitatives on the x and y axes, and an optional 3rd categorical or quantitative column for colour (if you use a quantitative for colour, specify to use a continuous colour scale).
-    - line plot: must have a DateTime column on the x-axis, a quantitative on the y axis, and an optional 3rd categorical column for colour.
-    - box plots: must have a categorical on the x-axis, a quantitative on the y axis, and an optional 3rd categorical column for separate y-axes or grouped box plots, or an optional 3rd quantitative column to be plotted on a separate y axis.
-    - histogram: must have a quantitative on the x-axis in bins, counts on the y axis, and an optional 2nd categorical column for stacked bars, multiple axes, or colour.
-    - bar chart: must have a categorical on the x-axis and counts on the y axis, and an optional 3rd categorical column for stacked bars or multiple axes.
+    - scatter plots: must have only quantitatives on the x and y axes, and an optional 3rd categorical with n_distinct <= 3 or quantitative column for colour (if you use a quantitative for colour, specify to use a continuous colour scale).
+    - line plot: must have a DateTime column on the x-axis, a quantitative on the y axis, and an optional 3rd categorical column with n_distinct <= 3 for colour.
+    - box plots: must have a categorical on the x-axis, a quantitative on the y axis, and an optional 3rd categorical column with n_distinct <= 3 for separate y-axes or grouped box plots, or an optional 3rd quantitative column to be plotted on a separate y axis.
+    - histogram: must have a quantitative on the x-axis in bins, counts on the y axis, and an optional 2nd categorical column with n_distinct <= 3 for stacked bars, multiple axes, or colour.
+    - bar chart: must have a categorical on the x-axis and counts on the y axis, and an optional 3rd categorical column with n_distinct <= 3 for stacked bars or multiple axes.
 
   3. **Column Types**:
     - Quantitative variables must be numeric or integers and must not have the type 'Categorical' in the summary.
     - Categorical variables must have the type 'Categorical' in the summary.
     - For columns with the type 'Large Categorical' or 'Free Text', specify how to select the 10 most interesting categories (e.g. the 10 most expensive car brands).
     - Any variables referenced in plot descriptions must also be in the provided summary and list of input columns. Never reference a variable that does not correspond to a specific column.
+    - n_distinct in the summary must be <= 3 to use a column for additional colouring, grouping, or separate y-axes to avoid visual clutter.
 
 *** OUTPUT RULES ***
 - Your response must only be a JSON string, formatted as a fenced JSON code block, with the following keys:
@@ -80,17 +85,18 @@ describe_custom_dashboard_prompt <- "Design this dashboard consisting of %d plot
 *** DASHBOARD RULES ***
   1. Make sure all plots can be plotted as `ggplot` objects.
   2. **Plot Types**:
-    - scatter plots: must have only quantitatives on the x and y axes, and an optional 3rd categorical or quantitative column for colour (if you use a quantitative for colour, specify to use a continuous colour scale).
-    - line plot: must have a DateTime column on the x-axis, a quantitative on the y axis, and an optional 3rd categorical column for colour.
-    - box plots: must have a categorical on the x-axis, a quantitative on the y axis, and an optional 3rd categorical column for separate y-axes or grouped box plots, or an optional 3rd quantitative column to be plotted on a separate y axis.
-    - histogram: must have a quantitative on the x-axis in bins, counts on the y axis, and an optional 2nd categorical column for stacked bars, multiple axes, or colour.
-    - bar chart: must have a categorical on the x-axis and counts on the y axis, and an optional 3rd categorical column for stacked bars or multiple axes.
+    - scatter plots: must have only quantitatives on the x and y axes, and an optional 3rd categorical with n_distinct <= 3 or quantitative column for colour (if you use a quantitative for colour, specify to use a continuous colour scale).
+    - line plot: must have a DateTime column on the x-axis, a quantitative on the y axis, and an optional 3rd categorical column with n_distinct <= 3 for colour.
+    - box plots: must have a categorical on the x-axis, a quantitative on the y axis, and an optional 3rd categorical column with n_distinct <= 3 for separate y-axes or grouped box plots, or an optional 3rd quantitative column to be plotted on a separate y axis.
+    - histogram: must have a quantitative on the x-axis in bins, counts on the y axis, and an optional 2nd categorical column with n_distinct <= 3 for stacked bars, multiple axes, or colour.
+    - bar chart: must have a categorical on the x-axis and counts on the y axis, and an optional 3rd categorical column with n_distinct <= 3 for stacked bars or multiple axes.
 
   3. **Column Types**:
     - Quantitative variables must be numeric or integers and must not have the type 'Categorical' in the summary.
     - Categorical variables must have the type 'Categorical' in the summary.
     - For columns with the type 'Large Categorical' or 'Free Text', specify how to select the 10 most interesting categories (e.g. the 10 most expensive car brands).
     - Any variables referenced in plot descriptions must also be in the provided summary and list of input columns. Never reference a variable that does not correspond to a specific column.
+    - n_distinct in the summary must be <= 3 to use a column for additional colouring, grouping, or separate y-axes to avoid visual clutter.
 
 *** OUTPUT RULES ***
 - Your response must only be a JSON string, formatted as a fenced JSON code block, with the following keys:
@@ -125,15 +131,15 @@ improve_dashboard_prompt <- "Can you improve the previous response by replacing 
 1. **Compliance with DASHBOARD RULES**:
    - Ensure all plots adhere to the previously stated DASHBOARD RULES.
    - Replace any plots that do not follow these rules.
+   - Remove any additional categorical columns used for colouring, grouping, or separate y-axes, which have n_distinct > 3 to avoid visual clutter
 
 2. **Logical Sense**:
    - Replace any plots that do not make logical sense (e.g. a plot using an ID number as a continuous value).
 
 3. **Visual Enhancements**:
    - Where appropriate and not present already, add or improve existing visual enhancements:
-     - Add categorical columns from the summary if they are logically relevant to show relationships across different categories. Explain how these should be used.
+     - If they have at most 3 distinct categories (n_distinct <= 3) and are logically relevant, add categorical columns from the summary to show relationships across different categories. Explain how these should be used.
      - For large categorical variables (e.g. more than 10 categories), avoid using them for colour or separate y-axes. Instead, summarise the data by selecting the top categories based on a relevant metric (e.g. top 10 regions by sales).
-     - Group related plots together into one plot. For example, if there are two box/bar plots with the same categories, use multiple separate y-axes to plot them as a single `ggplot` (ensure not to include a large number of separate y-axes).
      - Add 95%% prediction ellipses per category for any scatter plots that have a categorical column for colour.
      - Add lines of best fit with 95%% confidence intervals to line plots, and scatter plots without categories.
      - Be creative to make the plots more interesting and insightful.
